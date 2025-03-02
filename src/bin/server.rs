1f470eed785f9f627d3e6d882adab6f9255502cf
jusikXL <125464973+jusikXL@users.noreply.github.com>
2025-03-03 00:11:25 +0100

    improve error handling

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -16,10 +16,10 @@ use futures_util::{SinkExt, StreamExt};
 use tokio_tungstenite::tungstenite::{Error as TungsteniteError, Message};
 
 #[derive(Debug)]
-enum ServerError {
+pub enum ServerError {
     UserAlreadyJoined,
     NonExistentChannel,
-    NonExistentUser,
+    UserNotJoined,
     MessagePassing(SendError<Message>),
     WebSocketError(TungsteniteError),
     JoinError(JoinError),
@@ -77,8 +77,7 @@ where
 
         let outgoing_task = tokio::spawn(async move {
             while let Some(msg) = rx.recv().await {
-                // if we receive message from other clients
-                // send them down the stream
+                // if we receive message, send them down the stream
                 if let Err(e) = outgoing.send(msg).await {
                     return Err(ServerError::WebSocketError(e));
                 }
@@ -96,6 +95,9 @@ where
                         if let Err(e) = Self::join(addr, tx.clone(), channel, &channels) {
                             tx.send(Message::text(format!("Error joining channel: {e:?}")))
                                 .map_err(|e| ServerError::MessagePassing(e))?;
+                        } else {
+                            tx.send(Message::text(format!("Joined!")))
+                                .map_err(|e| ServerError::MessagePassing(e))?;
                         }
                     } else if text.starts_with("/leave") {
                         let channel = text[6..].trim().to_string();
@@ -103,12 +105,18 @@ where
                         if let Err(e) = Self::leave(&addr, &channel, &channels) {
                             tx.send(Message::text(format!("Error leaving channel: {e:?}")))
                                 .map_err(|e| ServerError::MessagePassing(e))?;
+                        } else {
+                            tx.send(Message::text(format!("Left!")))
+                                .map_err(|e| ServerError::MessagePassing(e))?;
                         }
                     } else if text.starts_with("/send") {
                         let parts: Vec<&str> = text[5..].trim().splitn(2, ' ').collect();
 
                         if let Err(e) = Self::send(&addr, parts[1], parts[0], &channels) {
                             tx.send(Message::text(format!("Error sending message: {e:?}")))
+                                .map_err(|e| ServerError::MessagePassing(e))?;
+                        } else {
+                            tx.send(Message::text(format!("Sent!")))
                                 .map_err(|e| ServerError::MessagePassing(e))?;
                         }
                     } else {
@@ -126,7 +134,8 @@ where
 
         outgoing_task
             .await
-            .map_err(|e| ServerError::JoinError(e))??; // not sure how to handle it here
+            .map_err(|e| ServerError::JoinError(e))??;
+
         Ok(())
     }
 
@@ -137,7 +146,6 @@ where
         channels: &Channels,
     ) -> Result<(), ServerError> {
         let mut channels = channels.lock().unwrap();
-        println!("{user_addr} joins {channel}");
 
         match channels.get_mut(&channel) {
             // Channel has already been created
@@ -174,7 +182,6 @@ where
         channels: &Channels,
     ) -> Result<(), ServerError> {
         let mut channels = channels.lock().unwrap();
-        println!("{user_addr} leaves {channel}");
 
         match channels.get_mut(channel) {
             // Channel has been created
@@ -183,7 +190,7 @@ where
                     // User has been removed
                     Some(_) => Ok(()),
                     // Not found user
-                    None => Err(ServerError::NonExistentUser),
+                    None => Err(ServerError::UserNotJoined),
                 }
             }
             // Channel does not exist
@@ -201,20 +208,27 @@ where
         channels: &Channels,
     ) -> Result<(), ServerError> {
         let channels = channels.lock().unwrap();
-        println!("{user_addr} sends {message} to {channel}");
 
         match channels.get(channel) {
             // Channel has been created
             Some(users) => {
-                // Broadcast message to all users except this
-                for (peer_addr, peer_tx) in users {
-                    if peer_addr != user_addr {
-                        peer_tx
-                            .send(Message::text(message.to_string()))
-                            .map_err(|e| ServerError::MessagePassing(e))?
+                match users.get(&user_addr) {
+                    // If user has joined the channel
+                    Some(_) => {
+                        // Broadcast message to all users except this
+                        for (peer_addr, peer_tx) in users {
+                            if peer_addr != user_addr {
+                                peer_tx
+                                    .send(Message::text(message.to_string()))
+                                    .map_err(|e| ServerError::MessagePassing(e))?
+                            }
+                        }
+
+                        Ok(())
                     }
+                    // If user has not joined the channel
+                    None => Err(ServerError::UserNotJoined),
                 }
-                Ok(())
             }
             // Channel does not exist
             None => {
