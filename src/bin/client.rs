87704249ae24afab91c84574c6328ccc0b0dc82c
Rus Omnipotent <125464973+jusikXL@users.noreply.github.com>
2025-03-05 00:25:03 +0100

    feat: basic client, basic server

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,29 +1,119 @@
-use std::error::Error;
+use futures_util::{SinkExt, StreamExt};
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt},
+    sync::mpsc::{self, error::SendError},
+    task::{JoinError, JoinHandle},
+};
 
-use tokio::net::{TcpStream, ToSocketAddrs};
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::{Error as TungsteniteError, Message},
+};
 
-pub struct Client<A: ToSocketAddrs> {
-    server: A,
+#[derive(Debug)]
+pub enum ClientError {
+    MessagePassing(SendError<Message>),
+    Join(JoinError),
+    WebSocket(TungsteniteError),
 }
 
-impl<A> Client<A>
-where
-    A: ToSocketAddrs,
-{
-    pub fn new(server: A) -> Self {
+pub struct Client {
+    server: String,
+}
+
+impl Client {
+    pub fn new(server: &str) -> Self {
+        let server = format!("ws://{server}");
+
         Client { server }
     }
 
-    pub async fn join(&self, channel: String) -> Result<(), Box<dyn Error>> {
+    pub async fn start(&self) -> Result<(), ClientError> {
+        let (tx, mut rx) = mpsc::unbounded_channel();
+
+        let (ws_stream, _) = connect_async(&self.server)
+            .await
+            .map_err(|e| ClientError::WebSocket(e))?;
+        let (mut outgoing, mut incoming) = ws_stream.split();
+
+        println!("Client: Running");
+
+        // read stdin
+        let stdin_to_tx: JoinHandle<Result<(), SendError<Message>>> = tokio::spawn(async move {
+            let mut stdin = io::stdin();
+
+            loop {
+                let mut buf = vec![0; 1024];
+
+                match stdin.read(&mut buf).await {
+                    Err(_) | Ok(0) => {
+                        continue; // not sure here
+                    }
+                    Ok(n) => buf.truncate(n),
+                };
+
+                let text = match String::from_utf8(buf) {
+                    Ok(text) => text,
+                    Err(_) => continue,
+                };
+
+                if let Err(e) = tx.send(Message::text(text)) {
+                    return Err(e);
+                }
+            }
+        });
+
+        // receive messages from stdin and send to sink
+        let rx_to_outgoing: JoinHandle<Result<(), TungsteniteError>> = tokio::spawn(async move {
+            while let Some(msg) = rx.recv().await {
+                outgoing.send(msg).await?
+            }
+
+            Ok(())
+        });
+
+        let incoming_to_stdout = tokio::spawn(async move {
+            while let Some(msg) = incoming.next().await {
+                match msg {
+                    Ok(Message::Text(text)) => {
+                        let mut stdout = io::stdout();
+
+                        stdout.write_all(text.as_bytes()).await.unwrap();
+                        stdout.write_all(b"\n").await.unwrap();
+                        stdout.flush().await.unwrap();
+                    }
+                    Ok(_) => {
+                        // ignore message of invalid format
+                    }
+                    Err(_e) => {
+                        // ignore messages that cannot be resolved
+                    }
+                }
+            }
+        });
+
+        // await each of the tasks, panic if necessary
+        stdin_to_tx
+            .await
+            .map_err(|e| ClientError::Join(e))?
+            .map_err(|e| ClientError::MessagePassing(e))?;
+
+        rx_to_outgoing
+            .await
+            .map_err(|e| ClientError::Join(e))?
+            .map_err(|e| ClientError::WebSocket(e))?;
+
+        incoming_to_stdout.await.map_err(|e| ClientError::Join(e))?;
+
         Ok(())
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let client = Client::new("127.0.0.1:4000");
+    let client = Client::new("127.0.0.1:8080");
 
-    if let Err(e) = client.join("channel 1".to_string()).await {
-        eprint!("Client error: {e}");
+    if let Err(e) = client.start().await {
+        eprint!("Client error: {e:#?}");
     }
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,18 +1,36 @@
-use futures_util::StreamExt;
 use std::{
-    collections::HashSet,
-    error::Error,
+    collections::HashMap,
+    net::SocketAddr,
     sync::{Arc, Mutex},
-    time::Duration,
 };
-use tokio::net::{TcpListener, TcpStream, ToSocketAddrs};
 
-type Channels = Arc<Mutex<HashSet<String>>>;
+use tokio::{
+    net::{TcpListener, TcpStream, ToSocketAddrs},
+    sync::mpsc::{self, error::SendError, UnboundedSender},
+    task::JoinError,
+};
+
+use futures_util::{SinkExt, StreamExt};
+
+use tokio_tungstenite::tungstenite::{Error as TungsteniteError, Message};
+
+#[derive(Debug)]
+pub enum ServerError {
+    NotJoined,
+    AlreadyJoined,
+    NoSuchChannel,
+    MessagePassing(SendError<Message>),
+    WebSocket(TungsteniteError),
+    Join(JoinError),
+}
+
+type User = UnboundedSender<Message>;
+type Users = HashMap<SocketAddr, User>;
+type Channels = Arc<Mutex<HashMap<String, Users>>>; // addr => tx
 
 pub struct Server<A: ToSocketAddrs> {
     addr: A,
-    channels: Channels,
-    // will block the thread server and possible other tasks are on for some time
+    channels: Channels, // will block the real thread for some time
 }
 
 impl<A> Server<A>
@@ -22,44 +40,193 @@ where
     pub fn new(addr: A) -> Self {
         Self {
             addr,
-            channels: Arc::new(Mutex::new(HashSet::new())),
+            channels: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
-    pub async fn start(&self) -> Result<(), Box<dyn Error>> {
+    pub async fn start(&self) -> std::io::Result<()> {
         let listener = TcpListener::bind(&self.addr).await?;
         println!("Server: Listening");
 
-        while let Ok((stream, _)) = listener.accept().await {
+        while let Ok((stream, addr)) = listener.accept().await {
             let channels = Arc::clone(&self.channels);
 
+            // Spawn a separate task
             tokio::spawn(async move {
-                // process stream
-
-                Self::process_stream(stream, channels).await;
+                if let Err(e) = Self::handle_connection((stream, addr), channels).await {
+                    eprintln!("Error handling connection from {addr}: {e:#?}");
+                }
             });
         }
 
         Ok(())
     }
 
-    async fn process_stream(stream: TcpStream, _channels: Channels) {
-        let addr = stream
-            .peer_addr()
-            .expect("connected streams should have a peer address");
-        println!("Peer address: {}", addr);
-
+    async fn handle_connection(
+        (stream, addr): (TcpStream, SocketAddr),
+        channels: Channels,
+    ) -> Result<(), ServerError> {
         let ws_stream = tokio_tungstenite::accept_async(stream)
             .await
-            .expect("Error during the websocket handshake occurred");
+            .map_err(|e| ServerError::WebSocket(e))?;
 
-        println!("New WebSocket connection: {}", addr);
+        let (mut outgoing, mut incoming) = ws_stream.split();
 
-        let (write, read) = ws_stream.split();
-        // We should not forward messages other than text or binary.
-        read.forward(write)
-            .await
-            .expect("Failed to forward messages")
+        let (tx, mut rx) = mpsc::unbounded_channel();
+
+        let outgoing_task = tokio::spawn(async move {
+            while let Some(msg) = rx.recv().await {
+                // if we receive message, send them down the stream
+                outgoing
+                    .send(msg)
+                    .await
+                    .map_err(|e| ServerError::WebSocket(e))?
+            }
+
+            Ok(())
+        });
+
+        let send_message = |msg: &str, tx: &mpsc::UnboundedSender<Message>| {
+            tx.send(Message::text(msg))
+                .map_err(|e| ServerError::MessagePassing(e))
+        };
+
+        while let Some(msg) = incoming.next().await {
+            match msg {
+                Ok(Message::Text(text)) => {
+                    if text.starts_with("/join") {
+                        let channel = text[5..].trim().to_string();
+
+                        if let Err(e) = Self::join(addr, tx.clone(), channel, &channels) {
+                            send_message(&format!("Error joining channel: {e:?}"), &tx)?;
+                        } else {
+                            send_message("Joined!", &tx)?;
+                        }
+                    } else if text.starts_with("/leave") {
+                        let channel = text[6..].trim().to_string();
+
+                        if let Err(e) = Self::leave(&addr, &channel, &channels) {
+                            send_message(&format!("Error leaving channel: {e:?}"), &tx)?;
+                        } else {
+                            send_message("Left!", &tx)?;
+                        }
+                    } else if text.starts_with("/send") {
+                        let parts: Vec<&str> = text[5..].trim().splitn(2, ' ').collect();
+
+                        if parts.len() < 2 {
+                            send_message(
+                                "Invalid /send usage. Expecting <channel> <message>",
+                                &tx,
+                            )?;
+                        }
+
+                        if let Err(e) = Self::send(&addr, parts[1], parts[0], &channels) {
+                            send_message(&format!("Error sending message: {e:?}"), &tx)?;
+                        } else {
+                            send_message("Sent!", &tx)?;
+                        }
+                    } else {
+                        send_message("Unknown command", &tx)?;
+                    }
+                }
+                Ok(_) => send_message("Unknown command", &tx)?,
+                Err(e) => return Err(ServerError::WebSocket(e)),
+            }
+        }
+
+        outgoing_task.await.map_err(|e| ServerError::Join(e))??;
+
+        Ok(())
+    }
+
+    fn join(
+        user_addr: SocketAddr,
+        user_tx: User,
+        channel: String,
+        channels: &Channels,
+    ) -> Result<(), ServerError> {
+        let mut channels = channels.lock().unwrap();
+
+        match channels.get_mut(&channel) {
+            // Channel has already been created
+            Some(users) => {
+                match users.get_mut(&user_addr) {
+                    // User has already joined
+                    Some(_) => Err(ServerError::AlreadyJoined),
+                    // User has not joined
+                    None => {
+                        // Add user
+                        users.insert(user_addr, user_tx);
+                        Ok(())
+                    }
+                }
+            }
+            // Channel does not exist
+            None => {
+                // Create channel, add user
+                let mut users = HashMap::new();
+                users.insert(user_addr, user_tx);
+
+                channels.insert(channel, users);
+                Ok(())
+            }
+        }
+    }
+
+    fn leave(
+        user_addr: &SocketAddr,
+        channel: &str,
+        channels: &Channels,
+    ) -> Result<(), ServerError> {
+        let mut channels = channels.lock().unwrap();
+
+        match channels.get_mut(channel) {
+            // Channel has been created
+            Some(users) => {
+                match users.remove(user_addr) {
+                    // User has been removed
+                    Some(_) => Ok(()),
+                    // Not found user
+                    None => Err(ServerError::NotJoined),
+                }
+            }
+            // Channel does not exist
+            None => Err(ServerError::NoSuchChannel),
+        }
+    }
+
+    fn send(
+        user_addr: &SocketAddr,
+        message: &str,
+        channel: &str,
+        channels: &Channels,
+    ) -> Result<(), ServerError> {
+        let channels = channels.lock().unwrap();
+
+        match channels.get(channel) {
+            // Channel has been created
+            Some(users) => {
+                match users.get(&user_addr) {
+                    // If user has joined the channel
+                    Some(_) => {
+                        // Broadcast message to all users except this
+                        for (peer_addr, peer_tx) in users {
+                            if peer_addr != user_addr {
+                                peer_tx
+                                    .send(Message::text(message.to_string()))
+                                    .map_err(|e| ServerError::MessagePassing(e))?
+                            }
+                        }
+
+                        Ok(())
+                    }
+                    // If user has not joined the channel
+                    None => Err(ServerError::NotJoined),
+                }
+            }
+            // Channel does not exist
+            None => Err(ServerError::NoSuchChannel),
+        }
     }
 }
 
@@ -70,6 +237,6 @@ async fn main() {
     // start server on current thread
     // no specific new thread is created for that
     if let Err(e) = server.start().await {
-        eprintln!("Server Error: {e}");
+        eprintln!("Error starting the server: {e}");
     }
 }
